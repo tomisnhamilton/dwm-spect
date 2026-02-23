1a13752ade242fc0bee1c4f9ad4232b319922a0c
Tomis Hamilton <tomisnhamilton@gmail.com>
2026-02-22 23:33:50 -0700

    UPD: package listings

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 # Packages
 sudo pacman -Syyu
-sudo pacman -S xorg-server xorg-xsetroot xorg-zephyr xorg-twm dunst picom slock feh flameshot
+sudo pacman -S xorg-server xorg-xsetroot xorg-zephyr xorg-twm dunst picom slock feh flameshot ttf-hack-nerd ttf-jetbrains-mono-nerd mpd mpd-mpris
